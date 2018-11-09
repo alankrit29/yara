0d15cf2e27318efc5b3d9893ba982f61d02b086f
Alankrit Srivastava <alankrit.srivastava256@webkul.com>
2018-11-09 21:11:26 +0530

    Update mail.py

--- mail.py
+++ mail.py
@@ -62,5 +62,16 @@ msgq.add_header('Content-Disposition', 'attachment', filename=filename)
 msg.attach(msgq)
 qwertyuiop = msg.as_string()
 
+fp = open('/var/log/phpcs.log', 'rb')
+msgq = MIMEBase('text', 'text')
+msgq.set_payload(fp.read())
+fp.close()
+encoders.encode_base64(msgq)
+filename='phpcs.log'
+msgq.add_header('Content-Disposition', 'attachment', filename=filename)
+msg.attach(msgq)
+qwertyuiop = msg.as_string()
+
+
 session.sendmail(sys.argv[2], sys.argv[3], qwertyuiop)
 session.quit()
